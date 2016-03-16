66534f1b4607ed496717bd725dd3528e9fc3c6ca
joshmallow <dev.scheffner@outlook.de>
2016-03-16 13:35:57 +0100

    initialize npm
    add .gitignore filr
    
    passport-dwbn-oauth2.njsproj deleted online with Bitbucket
    
    provides user profile
    
    new test and error handling
    
    remove unnecessary test
    
    restore accidentially deleted tests
    
    bug fix: email address was hard coded
    
    fixed bug and linted oauth2.js
    
    Rafactor code, add eslint and istanbul
    
    Replace personal information with fake data

---
+++ test/module-test.js
@@ -0,0 +1,14 @@
+﻿/* eslint prefer-arrow-callback: off, func-names: off */
+
+const expect = require('chai').expect;
+const DWBNStrategy = require('../');
+
+describe('passport-dwbn-oauth2', () => {
+  it('should expose the strategy directly from the package', function () {
+    expect(typeof DWBNStrategy).to.equal('function');
+  });
+
+  it('should expose the strategy at .Strategy', function () {
+    expect(typeof DWBNStrategy.Strategy).to.equal('function');
+  });
+});
---
+++ test/oauth2-test.js
@@ -0,0 +1,58 @@
+﻿/* eslint prefer-arrow-callback: off, func-names: off */
+
+const expect = require('chai').expect;
+const DWBNStrategy = require('../');
+const OAuth2Strategy = require('passport-oauth2');
+const testUser = require('./test-user-data.json');
+
+const options = {
+  clientID: 'T3STID',
+  clientSecret: 'psst'
+};
+
+const verify = () => {};
+
+const strategy = new DWBNStrategy(options, verify);
+
+describe('DWBN Strategy', function () {
+  it('should be named dwbn', function () {
+    expect(strategy.name).to.equal('dwbn');
+  });
+
+  it('should be a child of the OAuth Strategy', function () {
+    expect(strategy instanceof OAuth2Strategy).to.equal(true);
+  });
+
+  // TODO: Test default options
+
+  describe('#userProfile', function () {
+    it('should load profile', function (done) {
+      // eslint-disable-next-line no-underscore-dangle
+      strategy._oauth2.get = function (url, token, callback) {
+        const body = JSON.stringify(testUser);
+        callback(null, body);
+      };
+
+      strategy.userProfile('token', function (err, profile) {
+        if (err) {
+          return done(err);
+        }
+
+        const expectedProfile = {
+          provider: 'dwbn',
+          id: '1234',
+          displayName: 'Jon Doe',
+          name: {
+            familyName: 'Doe',
+            givenName: 'Jon'
+          },
+          emails: [
+            { value: 'admin@jon-doe.com' }
+          ]
+        };
+        expect(profile).to.eql(expectedProfile);
+        return done();
+      });
+    });
+  });
+});
